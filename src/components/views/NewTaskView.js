587d55f8162d99ab0f1243c7a1ace99cd1b1f65e
Haley <99759106+HLeung37@users.noreply.github.com>
2022-12-21 06:20:46 -0500

    Fixed adding new task
    Fixed 100%

--- src/components/views/NewTaskView.js
+++ src/components/views/NewTaskView.js
@@ -6,13 +6,19 @@ const NewTaskView = (props) => {
 
   return (
     <div className="root">
+      <nav class="navMenu">
+        <a href="http://localhost:3000/">Home</a>
+        <a href="http://localhost:3000/employees">Employees</a>
+        <a href="http://localhost:3000/tasks">Tasks</a>
+        <div class="dot"></div>
+      </nav>
       <div className="formContainer">
         <div className="formTitle">
           <h2>
             New Task
           </h2>
         </div>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <label>Description: </label>
           <input type="text" name="description" onChange ={(e) => handleChange(e)} />
           <br/>
